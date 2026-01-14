5eb2d11e6d818064c77e181e4bec0837fe7c9aac
Eden <rbahmaddzaki@gmail.com>
2026-01-14 14:51:34 +0700

    menambahkan parameter dan argumen

--- LatihanMethod1/Program.cs
+++ LatihanMethod1/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int c = 0;
+
+void tambah(int a, int b)
+{
+    c = a + b;
+    Console.WriteLine($"{a} + {b} = {c}");
+}
+void kurang(int a, int b)
+{
+    c = a - b;
+    Console.WriteLine($"{a} - {b} = {c}");
+}
+void kali(int a, int b)
+{
+    c = a * b;
+    Console.WriteLine($"{a} * {b} = {c}");
+}
+void bagi(int a, int b)
+{
+    c = a / b;
+    Console.WriteLine($"{a} / {b} = {c}");
+}
+tambah(1,1);
+kurang(100, 20);
+kali(1, 1);
+bagi(6, 7);
